3ae830f538f68b006198f6d4413030278ae0c7b7
Émile Perron <contact@emileperron.com>
2022-06-23 08:32:31 -0400

    feat: change php-cs-fixer ruleset to @PhpCsFixer (with a few exceptions)

--- replicate/.php-cs-fixer.dist.php
+++ replicate/.php-cs-fixer.dist.php
@@ -8,9 +8,19 @@ $finder = PhpCsFixer\Finder::create()
 
 $config = new PhpCsFixer\Config();
 return $config->setRules([
-        '@Symfony' => true,
+        '@PhpCsFixer' => true,
         'yoda_style' => ['equal' => false, 'identical' => false],
         'increment_style' => ['style' => 'post'],
+		'return_assignment' => false,
+		'heredoc_to_nowdoc' => false,
+		'php_unit_internal_class' => false,
+		'php_unit_test_class_requires_covers' => false,
+		'multiline_whitespace_before_semicolons' => false,
+		'method_argument_space' => false,
+		'single_quote' => false,
+		'escape_implicit_backslashes' => false,
+		'explicit_indirect_variable' => false,
+		'method_chaining_indentation' => false,
     ])
     ->setIndent("\t")
     ->setFinder($finder);
