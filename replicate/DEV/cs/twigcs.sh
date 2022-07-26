02b2154b710ec160bcbbb177da9bfe6e046fda29
Émile Perron <contact@emileperron.com>
2022-07-26 14:45:14 -0400

    fix: make scripts more friendly by default for non-Symfony projects (#13)

--- replicate/DEV/cs/twigcs.sh
+++ replicate/DEV/cs/twigcs.sh
@@ -1,2 +1,10 @@
-bin/console lint:twig templates
-vendor/friendsoftwig/twigcs/bin/twigcs templates
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
+
+if [ -d "${BASEDIR}/templates" ]; then
+	# Lint templates before validating them if possible
+	if [ -f "${BASEDIR}/bin/console" ]; then
+		bin/console lint:twig templates
+	fi
+
+	vendor/friendsoftwig/twigcs/bin/twigcs templates
+fi
