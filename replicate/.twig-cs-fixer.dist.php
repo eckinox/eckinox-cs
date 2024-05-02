37c1666ba5567fcc225fe61b0fa70a1b5362c81e
Emmanuelle Mercadal <mercadal.em@gmail.com>
2024-05-02 06:52:07 -0600

    feat: Update friendsofsymfony/twigcs rules

--- replicate/.twig-cs-fixer.dist.php
+++ replicate/.twig-cs-fixer.dist.php
@@ -3,6 +3,7 @@
 $ruleset = new TwigCsFixer\Ruleset\Ruleset();
 $ruleset->addStandard(new TwigCsFixer\Standard\TwigCsFixer());
 $ruleset->removeRule(TwigCsFixer\Rules\Whitespace\BlankEOFRule::class);
+$ruleset->removeRule(TwigCsFixer\Rules\String\SingleQuoteRule::class);
 
 $config = new TwigCsFixer\Config\Config();
 $config->allowNonFixableRules();
